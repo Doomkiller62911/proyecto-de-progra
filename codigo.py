fa2ec36b142f962a6eb39a497e89a2ad04f9ba08
DUM <106053263+Doomkiller62911@users.noreply.github.com>
2023-04-26 23:13:02 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,8 +5,13 @@ seat=2000
 time=2000
 movie=2000
 
-
+    
+nombre = input("Ingrese el nombre del cliente: ")
+cedula = int(input("Ingrese el número de cédula del cliente: "))
+correo = input("Ingrese su correo electrónico: ")
+personas = int(input("Ingrese la cantidad de personas para la reserva: "))
 total = seat + time + movie 
+total_completo = total * personas
 root = tk.Tk()
 root.title("Sistema de Reserva de Peliculas")
 
@@ -34,6 +39,17 @@ time_dropdown.pack(side=tk.LEFT)
 
 time_frame.pack()
 
+
+pay = tk.Frame(root, padx=10, pady=10)
+tpay = tk.Label(pay, text="Selecciona un metodo de pago:")
+tpay.pack(side=tk.LEFT)
+
+vpay = tk.StringVar()
+lpay = ["Tarjeta", "Efectivo","Simpe"]
+lpayd = tk.OptionMenu(pay, vpay, *lpay)
+lpayd.pack(side=tk.LEFT)
+pay.pack()
+
 seat_frame = tk.Frame(root, padx=10, pady=10)
 seat_label = tk.Label(seat_frame, text="Seleccione sus asientos:")
 seat_label.pack()
@@ -53,10 +69,27 @@ for row in range(num_rows):
 
 seat_frame.pack()
 
-def show_message():
-    tkinter.messagebox.showinfo("Factura", "La pelicula: 2000.                                                                               El horario: 2000.                                                                               El asiento: 2000.                                                                               TOTAL:6000")
+def factura():
+    label1 = tk.Label(root, text="El precio del asiento es: " + str(seat))
+    label2 = tk.Label(root, text="El precio por el horario es: " + str(time))
+    label3 = tk.Label(root, text="El precio por la pelicula es: " + str(movie))
+    label4 = tk.Label(root, text="El total a pagar es:  " + str(total_completo))
+    label5 = tk.Label(root, text="El nombre asiento es: " + str(nombre))
+    label6 = tk.Label(root, text="La cedula asignada es: " + str(cedula))
+    label7 = tk.Label(root, text="El correo asignado es:  " + str(correo))
+    label8 = tk.Label(root, text="La cantidad total de personas es:  " + str(personas))
 
-book_button = tk.Button(root, text="Pagar", padx=10, pady=10, command=show_message)
+    label1.pack()   
+    label2.pack()
+    label3.pack()
+    label4.pack()
+    label5.pack()   
+    label6.pack()
+    label7.pack()
+    label8.pack()
+    root.mainloop()
+
+book_button = tk.Button(root, text="Pagar", padx=10, pady=10, command=factura)
 
 book_button.pack()
 
@@ -70,3 +103,8 @@ def select_seat(seat_id):
 
 
 root.mainloop()
+
+root = tk.Tk()
+
+
+
